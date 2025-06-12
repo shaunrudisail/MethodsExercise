af082f6b9ba35ea8c9f2f240900818b5842a3851
Shaun Rudisail <sprudisail@gmail.com>
2025-06-11 22:22:17 -0400

    Completed Exercise 1 & 2 and Challenge Mode.

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace MethodsExercise
 {
     public class Program
-    { 
-        static void Main(string[] args)
+    {
+        public static void Storytime()
         {
             Console.WriteLine("What is your name?");
             
@@ -48,11 +48,55 @@
             Console.WriteLine("");
             Console.WriteLine($"Story time! I just met an amazing person named {name} and their favorite color is {color}. They love {animal}s. They love to eat {food}, just like me! They love to drive {car}s! And last but not least, their favorite genre of music is {genre}! Can you believe that?! And their favorite band in the {genre} style of music is {band}!");
             Console.WriteLine("");
-            Console.WriteLine($"One day, {name} decided that they would travel the country following {band} around on tour for a whole week straight! They knew this would be a long journey, so they grabbed their {animal}, hopped in their {car}, and stocked up on as much {food} as they possibly could!");
+            Console.WriteLine($"One day, {name} decided that they would travel the country following {band} around on tour for a whole week straight! They knew this would be a long journey, so they grabbed their {animal}, hopped in their {color} {car}, and stocked up on as much {food} as they possibly could!");
             Console.WriteLine("");
-            Console.WriteLine($"Now it was time to hit the road! They traveled to the first show of {band}'s tour and it was a LONG line to get tickets, but {name} was determined to see {band} play live on stage. And lucky for {name}, {band} loves {animal}s too and their show is 100% {animal}-friendly. {name} watched {band} play their whole set in its entirety every single night and ate {food} the entire time!");
+            Console.WriteLine($"Now it was time to hit the road! They traveled to the first show of {band}'s tour and it was a LONG line to get tickets, but {name} was determined to see {band} play live on stage. And lucky for {name}, {band} loves {animal}s, the color {color}, {car}s, and {food} too! {name} watched {band} play their whole set in its entirety every single night and ate {food} the entire time!");
             Console.WriteLine("");
-            Console.WriteLine($"As the tour came to a close, {name} gathered their things, their {animal}, hopped back in their {car} and headed back home!");
+            Console.WriteLine($"As the tour came to a close, {name} gathered their things, their {animal}, hopped back in their {color} {car} and headed back home!");
+        }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
+
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                //sum = sum + number;
+                sum += number;
+            }
+
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Storytime();
+            Console.WriteLine(Add(2, 2));
+            Console.WriteLine(Multiply(30, 2));
+            Console.WriteLine(Subtract(40, 12));
+            Console.WriteLine(Divide(160, 4));
+            Console.WriteLine(Sum(5, 5, 5));
+            Console.WriteLine(Sum(100, 100, 100));
+            Console.WriteLine(Sum(85, 79, 120, 36, 59));
         }
     }
 }
--- MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879bc51ef403450a0a853307a5e2bad9e738048e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206d81ed1b2658c25f733db9ba184baa21779c2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
